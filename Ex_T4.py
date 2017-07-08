b871ec52a832168a9157d82ff7c6cd0ee9d425d7
DolgayaMaria <dolgayam@gmail.com>
2017-07-08 11:32:58 +0300

    Update Ex_T4.py

--- Ex_T4.py
+++ Ex_T4.py
@@ -1,11 +1,10 @@
-w = 'a a a c c g t g g a c t'
-a = []
-w = w.split()
-for i in range (0,len(w)):
-    if w[i] not in a:
-        a.append(w[i])
-print(a)
-    
+def F(w):
+    a = []
+    w = w.split()
+    for i in range (0,len(w)):
+        if w[i] not in a:
+            a.append(w[i])
+    return a
 
     
               
